353abf9fe01456f6b4c3033526fc159d9b78496f
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2015-10-31 21:18:48 -0300

    Create Quicksort-inplace.cpp

---
+++ Quicksort-inplace.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int partitio(int A[], int lo, int hi)
+{
+    int pivot = A[hi];
+    int i = lo;
+    for (int j=lo;j<hi;j++)
+    {
+        if (A[j] <= pivot)
+        {
+            swap(A[i],A[j]);
+            i++;
+        }
+    }
+    swap(A[i],A[hi]);
+    for(int i=0;i<7;i++) cout<<A[i]<<" ";
+    cout<<endl;
+    return i;
+}
+void quicksort(int A[],int lo,int hi)
+{
+    if (lo < hi)
+    {
+        int p = partitio(A, lo, hi);
+        quicksort(A, lo, p - 1);
+        quicksort(A, p + 1, hi);
+    }
+}
+
+int main() {
+    int n; cin>>n;
+    int vet[n];
+    for(int i=0;i<n;i++) cin>>vet[i];
+    quicksort(vet,0,n-1);
+    return 0;
+}
