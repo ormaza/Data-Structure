bba2da0ec95086d74495dea517b7f53aae78a230
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2018-02-20 14:45:15 -0300

    Create MergeSort.js

---
+++ MergeSort.js
@@ -0,0 +1,63 @@
+var merge = function(array, p, q, r) {
+    var lowHalf = [];
+    var highHalf = [];
+
+    var k = p;
+    var i;
+    var j;
+    for (i = 0; k <= q; i++, k++) {
+        lowHalf[i] = array[k];
+    }
+    for (j = 0; k <= r; j++, k++) {
+        highHalf[j] = array[k];
+    }
+
+    k = p;
+    i = 0;
+    j = 0;
+    
+    // Repeatedly compare the lowest untaken element in
+    //  lowHalf with the lowest untaken element in highHalf
+    //  and copy the lower of the two back into array
+    while(i < lowHalf.length && j < highHalf.length){
+        if(lowHalf[i]<highHalf[j]){
+            array[k]=lowHalf[i];
+            i++;
+        } else {
+            array[k]=highHalf[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Once one of lowHalf and highHalf has been fully copied
+    //  back into array, copy the remaining elements from the
+    //  other temporary array back into the array
+    
+    while(i < lowHalf.length){
+        array[k]=lowHalf[i];   
+        i++; 
+        k++;
+    }
+    while(j < highHalf.length){
+        array[k]=highHalf[j];
+        j++; 
+        k++;
+    }
+};
+
+var mergeSort = function(array, p, r) {
+    
+    if(r>p) {
+        var q = floor((r-p)/2)+p;
+        mergeSort(array,p,q);
+        mergeSort(array,q+1,r);
+        merge(array, p, q, r);
+    }
+    
+};
+
+var array = [14, 7, 3, 12, 9, 11, 6, 2];
+mergeSort(array, 0, array.length-1);
+println("Array after sorting: " + array);
+Program.assertEqual(array, [2, 3, 6, 7, 9, 11, 12, 14]);
